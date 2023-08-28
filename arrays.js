8c750aef038c06694011fc20158148e80d72b7b4
Eric Batiste <ericbatiste4@gmail.com>
2023-08-28 16:04:52 -0600

    Demonstrate index positions

--- arrays.js
+++ arrays.js
@@ -10,3 +10,12 @@ evenNums.push(12);
 
 // .pop removes whatever item is last in the array and returns that item.
 booleans.pop();
+
+// Each item inside a given array has an index position 
+// that corresponds with a number. You always start 
+// counting from 0 so the first item in the array has an index of 0,
+// the second item has an index of 1, the third 2 and so on.
+
+// For example:
+console.log(cities[4]);
+// prints "Charelston"
