2aee6d863b1f2969c25d406fcb058e6044835a1c
Eric Batiste <ericbatiste4@gmail.com>
2023-08-28 15:52:00 -0600

    Add and explain array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
-var family = ["Doris", "Katie", "Daniel", "Lynne", "Amanda"];
+var cities = ["Denver", "Austin", "Richmond", "New York", "Charelston"];
 var evenNums = [2, 4, 6, 8, 10];
 var booleans = [true, true, false, false]
+
+// .indexOf returns the indexed position of the array item passed as an argument. In this case, 2.
+cities.indexOf("Richmond");
+
+// .push adds whatever argument that is passed to the end of the array.
+evenNums.push(12);
+
+// .pop removes whatever item is last in the array and returns that item.
+booleans.pop();
